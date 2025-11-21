78815158604e0f9a67a2ee13db22156761e70370
Alejandro Rodríguez Iglesias <arodriguezi10@iesagora.caceres>
2025-11-21 14:00:49 +0100

    Subtitulo añadido

--- subir_Git.ps1
+++ subir_Git.ps1
@@ -5,11 +5,12 @@ git add .
 
 Write-Host "Dime el comentario que le quieres añadir: "
 $comentario = Read-Host
-
 Write-Host "2. Creando el commit con el mensaje: $comentario"
 git commit -m $comentario
 
-Write-Host "3. Haciendo push a la rama principal (main) del repositorio remoto ..."
-git push origin main    
+Write-Host "Dime el nombre de la rama a la que quieres hacer push"
+$colorReloj = Read-Host
+Write-Host "3. Haciendo push a la rama $colorReloj  del repositorio remoto ..."
+git push origin $colorReloj
 
 Write-Host "Proceso de subida a Git finalizado."    
